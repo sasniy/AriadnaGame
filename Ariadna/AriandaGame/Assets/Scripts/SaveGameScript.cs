8530779c8ce7ae3ba0387af3acabe316dad887a3
sasniy <54496303+sasniy@users.noreply.github.com>
2021-06-06 17:48:23 +0500

    Небольшие изменения

---
+++ Ariadna/AriandaGame/Assets/Scripts/SaveGameScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SaveGameScript : MonoBehaviour
+{
+    public void SaveGame()
+    { 
+    }
+
+    public void LoadGame()
+    {
+
+    }
+}
